c684b781ff4234b61016ad040a7fe2f59fc2d363
Trish Ogen <patriciaogen@gmail.com>
2019-05-21 22:02:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, hash|
+    puts holiday_hash[season]
+    puts holiday_hash[hash]
 end
 
 def all_holidays_with_bbq(holiday_hash)
