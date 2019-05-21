a290edf49d05ea24977fa15da321393dc29b5ac4
Trish Ogen <patriciaogen@gmail.com>
2019-05-21 22:35:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,5 +69,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_names = []
+  holiday_hash.each do |season, hash|
+    hash.each do |holiday, holiday_hash|
+      if holiday_hash.include? 'BBQ'
+        holiday_names << holiday
+      end
+    end
+  end
+  holiday_names
 end
