7b10c32372926077a14f3dcd0ce380dde0e1b433
Trish Ogen <patriciaogen@gmail.com>
2019-05-21 21:57:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,13 +42,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   holiday_hash.each do |season, hash|
     if season == season
-      holiday_hash[season].each do |holiday, value|
-        if holiday == holiday_name
-          value = supply_array
-        end
+      holiday_hash[season][holiday_name] = supply_array
       end
     end
-  end
   holiday_hash
 end
 
